f446d5fcea4375e0e6fb50bc5064f104a27cfa39
Jonathan Dickerson <jongdickerson@gmail.com>
2014-09-03 19:28:46 -0400

    chartjs is functional

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
   var menu = $('#navigation-menu');
   var menuToggle = $('#js-mobile-menu');
@@ -11,13 +13,43 @@ $(document).ready(function() {
       }
     });
   });
-
-  // underline under the active nav item
-  $(".nav .nav-link").click(function() {
-    $(".nav .nav-link").each(function() {
-      $(this).removeClass("active-nav-item");
-    });
-    $(this).addClass("active-nav-item");
-    $(".nav .more").removeClass("active-nav-item");
-  });
 });
+
+
+
+// Get the context of the canvas element we want to select
+// var ctx = document.getElementById("myScore").getContext("2d");
+// var myNewChart = new Chart(ctx).PolarArea(data);
+
+// Get context with jQuery - using jQuery's .get() method.
+var ctx = $("#myScore").get(0).getContext("2d");
+// This will get the first returned node in the jQuery collection.
+// var myNewChart = new Chart(ctx);
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
+
+
+
+// And for a doughnut chart
+var myDoughnutChart = new Chart(ctx).Doughnut(data);
+
+
