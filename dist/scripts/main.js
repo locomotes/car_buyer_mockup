019108146af2403edeff656a88b2181498c2ad10
Jonathan Dickerson <jongdickerson@gmail.com>
2014-09-03 22:09:29 -0400

    build before merge

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -28,23 +28,23 @@ var ctx = $("#myScore").get(0).getContext("2d");
 
 var data = [
     {
-        value: 300,
+        value: 15,
         color:"#F7464A",
         highlight: "#FF5A5E",
         label: "Red"
     },
     {
-        value: 50,
+        value: 85,
         color: "#46BFBD",
         highlight: "#5AD3D1",
         label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
     }
+    // {
+    //     value: 100,
+    //     color: "#FDB45C",
+    //     highlight: "#FFC870",
+    //     label: "Yellow"
+    // }
 ]
 
 
